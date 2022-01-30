db27c9462f4318e9d266895d6a6e6e0baea31d90
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-01-30 06:16:26 +0000

    added gallery schema

--- cdg/schemas/gallery.js
+++ cdg/schemas/gallery.js
@@ -40,6 +40,150 @@ export default {
       },
     },
     {
+      name: 'image3',
+      title: 'Image 3',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image4',
+      title: 'Image 4',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image5',
+      title: 'Image 5',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image6',
+      title: 'Image 6',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image7',
+      title: 'Image 7',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image8',
+      title: 'Image 8',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image9',
+      title: 'Image 9',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image10',
+      title: 'Image 10',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image11',
+      title: 'Image 11',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image12',
+      title: 'Image 12',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image13',
+      title: 'Image 13',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image14',
+      title: 'Image 14',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image15',
+      title: 'Image 15',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image16',
+      title: 'Image 16',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image17',
+      title: 'Image 17',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image18',
+      title: 'Image 18',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image19',
+      title: 'Image 19',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'image20',
+      title: 'Image 20',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
       name: 'categories',
       title: 'Categories',
       type: 'array',
