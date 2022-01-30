06070153ce2b4ccb9ed01d9a2066598e7fd8378b
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-01-30 07:52:04 +0000

    navbar functionality compelte

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div>
-      <h1>NAVBAAAAAAAAR</h1>
-    </div>
+    <nav>
+      <div>
+        <Link to="/">
+          <h1>CDG</h1>
+        </Link>
+      </div>
+      <div>
+        <div>
+          <Link to="/galleries">Galleries</Link>
+          <Link to="/about">About</Link>
+        </div>
+      </div>
+    </nav>
   )
 }
 
