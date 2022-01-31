0a17e9a2cc90ed0cc64ee6c768679b1a54969de2
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-01-31 05:52:53 +0000

    successfully rendered out gallery schema

--- src/components/Galleries.js
+++ src/components/Galleries.js
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import sanityClient from '../sanity'
+import { Link } from 'react-router-dom'
 
 function Galleries() {
+  const [postData, setPostData] = useState(null)
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `
+         *[_type == "gallery"]{
+  _id,
+  title,
+  slug,
+image1{
+  asset -> {
+  _id,
+  url
+}
+},
+categories,
+publishedAt,
+  body,
+  author -> {
+  name,
+  image
+}
+}`
+      )
+      .then((data) => setPostData(data))
+      .catch(console.error)
+  }, [])
+
+  console.log(`MOOSE Knucle >>>`, postData)
+
   return (
-    <div>
-      <h1>This is the galleries page</h1>
-    </div>
+    <main>
+      <section>
+        <h1>Posts</h1>
+        <h2>Posts n ting</h2>
+        <div>
+          {postData &&
+            postData.map((post, index) => (
+              <article>
+                <Link to={'/blog/' + post.slug.current} key={post.slug.current}>
+                  <span key={index}>
+                    <img src={post.image1.asset.url} alt={post.image1.alt} />
+
+                    <span>
+                      <h3>{post.title}</h3>
+                    </span>
+                  </span>
+                </Link>
+              </article>
+            ))}
+        </div>
+      </section>
+    </main>
   )
 }
 
