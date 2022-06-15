5df30dc4e15844d9c32fb8ade8bf88f3b07cb833
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-06-16 00:35:21 +0100

    updated home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,22 +16,16 @@ function NavBar() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <LinkContainer to="/blog">
-                <Nav.Link>
-                  <i className="fas fa-shopping-cart" /> Blog
-                </Nav.Link>
+                <Nav.Link>Blog</Nav.Link>
               </LinkContainer>
 
               <Nav title="" id="username">
                 <LinkContainer to="/galleries">
-                  <Nav.Link>
-                    <i className="fas fa-user" /> Galleries
-                  </Nav.Link>
+                  <Nav.Link>Galleries</Nav.Link>
                 </LinkContainer>
 
                 <LinkContainer to="/about">
-                  <Nav.Link onClick="">
-                    <i className="fas fa-sign-out-alt" /> About
-                  </Nav.Link>
+                  <Nav.Link>About</Nav.Link>
                 </LinkContainer>
               </Nav>
             </Nav>
